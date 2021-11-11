b5f7673173bc7c515cec83e29d4f72247addbf79
Gianni Mondello <gianni.mondello@objectmethod.it>
2021-11-11 14:51:18 +0100

    aggiunti stili pagina singolo post

--- src/pages/PostDetail/PostDetail.js
+++ src/pages/PostDetail/PostDetail.js
@@ -1,3 +1,5 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
@@ -12,25 +14,42 @@ const PostDetail = (props) => {
   // componentDidMount
   useEffect(() => {
     getCurrentPost(id)
+    
   
   }, [id]);
   const getCurrentPost = (id) => {
     axios.get(`http://localhost:1337/posts/${id}`).then((resp) => {
       setPost(resp.data)
       
+      
     })
   }
+  const formatDate = (date) => {
+    if(date){
+    let newDate = date.split('T')[0]
+    return newDate.split("-").reverse().join("-")
+    }
+  }
+  
+   let category = ""
+   if(Object.keys(post).length > 0){
+    category = post.category.name
+   }
 
   return (
     <div className="post-detail">
       <div className="exit-button-wrapper">
       <Link className="exit-button" transition="fold-top" to="/posts">
-          <p>X</p>
+      <FontAwesomeIcon icon={faTimes} size="lg" />
         </Link>
       </div>
       <div className="text-wrapper">
-      <h1 className="title">{post.Title}</h1>
-      <p className="text">{post.Text}</p>
+      <h1 className="detail-title">{post.Title}</h1>
+      <p className="detail-text">{post.Text}</p>
+      </div>
+      <div className="detail-data-wrapper">
+        <span className="data-detail"><strong>Data di pubblicazione:</strong> {formatDate(post.published_at)}</span>
+        <span className="data-detail"><strong >Categoria:</strong> {category}</span>
       </div>
     </div>
   )
