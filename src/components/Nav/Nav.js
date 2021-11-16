2602d18c8b45b895b57452be7bc60ddbf0148ee3
Gianni Mondello <gianni.mondello@objectmethod.it>
2021-11-16 21:02:00 +0100

    fix rel

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -11,7 +11,7 @@ const Nav = () => {
       <Link transition="fold-top" to="/posts">
         <p>Posts</p>
       </Link>
-      <a href="https://salty-taiga-14197.herokuapp.com/admin/auth/login" target="_blank">Login</a>
+      <a href="https://salty-taiga-14197.herokuapp.com/admin/auth/login" target="_blank" rel="noreferrer">Login</a>
     </Menu>
   );
 };
