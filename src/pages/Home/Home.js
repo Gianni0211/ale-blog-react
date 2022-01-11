c629b237293a7112bd441e4a6a9a3dfeb92992c1
Gianni Mondello <gianni.mondello@objectmethod.it>
2022-01-11 15:25:00 +0100

    fix mobile home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,17 +6,18 @@ const Home = () => {
   return (
     <div className="home">
       <Nav />
-      {/* <div className="title-wrapper">
-        <Title size="400" text1="Arktista" text2="Unknown" open={true} />
-      </div> */}
-      <p className="home-text">
-        The lie came to earth to deceive humanity , but before it saw that
-        humanity is light, and it told the lie: <br /> “The humanity is light
-        but they are ignorant; so I will dress like the truth, I will walk like
-        the truth, I will speak like the truth and they will adore me ”. <br />
-        Then the lie saw the truth and the lie prostrated itself before it!
-      </p>
-      <img src={earth} className="earth-img" alt="earth-img"></img>
+
+      <div className="content-wrapper">
+        <p className="home-text">
+          The lie came to earth to deceive humanity , but before it saw that
+          humanity is light, and it told the lie: <br /> “The humanity is light
+          but they are ignorant; so I will dress like the truth, I will walk
+          like the truth, I will speak like the truth and they will adore me ”.{" "}
+          <br />
+          Then the lie saw the truth and the lie prostrated itself before it!
+        </p>
+        <img src={earth} className="earth-img" alt="earth-img"></img>
+      </div>
     </div>
   );
 };
