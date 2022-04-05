9cd74a42d36d11473a050aab4d0cbdbb9fc1403e
Gianni Mondello <gianni.mondello@objectmethod.it>
2022-04-05 10:45:06 +0200

    fix url immagini dei post

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,23 +1,19 @@
-import { Link } from 'react-tiger-transition'
-import Thumbnail from '../Thumbnail/Thumbnail'
-import './PostCard.css'
+import { Link } from "react-tiger-transition";
+import Thumbnail from "../Thumbnail/Thumbnail";
+import "./PostCard.css";
 
 const PostCard = (props) => {
-  const post = props.post
-  let id = post.id
-
+  const post = props.post;
+  let id = post.id;
   return (
     <Link className="card" transition="fold-top" to={`post/${id}`}>
-      {/* <div className="card-overlay">
-        <div className="card-overlay-link">Scopri di più</div>
-      </div> */}
       <Thumbnail image={post.Image} />
       <div className="card-text-wrapper">
         <p className="card-title">{post.Title}</p>
         <p className="card-slug">{post.Slug}</p>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -1,20 +1,20 @@
-import { STRAPI_BASE_URL } from '../../consts/consts'
-import './Thumbnail.css'
+import { STRAPI_BASE_URL } from "../../consts/consts";
+import "./Thumbnail.css";
 
 const Thumbnail = (props) => {
   /* console.log(props.image.formats.thumbnail.url) */
-  let hasImage = props.image ? true : false
-  
-  let src = ''
+  let hasImage = props.image ? true : false;
+
+  let src = "";
   hasImage
-    ? (src = `${STRAPI_BASE_URL}${props.image.formats.thumbnail.url}`)
-    : (src = 'https://via.placeholder.com/200')
+    ? (src = `${props.image.formats.thumbnail.url}`)
+    : (src = "https://via.placeholder.com/200");
 
   return (
     <div>
       <img className="card-img" alt="post-card-img" src={src} />
     </div>
-  )
-}
+  );
+};
 
-export default Thumbnail
+export default Thumbnail;
